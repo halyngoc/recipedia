5e8ee5c23574ed656c4ccc8d3052d36f7132792e
Ha Ly <lyha@pdx.edu>
2020-05-15 22:51:08 -0700

    Updates

---
+++ precache-manifest.4b0804fd35622b3df4e9cfd52bec11f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f18f65c2b14b3bd0d36c05428b92002",
+    "url": "/recipedia/index.html"
+  },
+  {
+    "revision": "5d6ec61ccb72118f682d",
+    "url": "/recipedia/static/js/2.26fccf9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipedia/static/js/2.26fccf9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac2a3438275449a6a75",
+    "url": "/recipedia/static/js/main.c4fad6e2.chunk.js"
+  },
+  {
+    "revision": "856688276154979f23b4",
+    "url": "/recipedia/static/js/runtime-main.942e0537.js"
+  }
+]);
