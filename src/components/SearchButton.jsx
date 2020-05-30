c648633988390a9215aa494f38c544bed521b58f
Ha Ly <lyha@pdx.edu>
2020-05-30 13:02:45 -0700

    Refactor IconButton to Button.jsx

--- src/components/SearchButton.jsx
+++ src/components/SearchButton.jsx
@@ -2,23 +2,7 @@ import React from 'react'
 import 'boxicons'
 import { theme } from '../global'
 import { useDevice } from '../util'
-import styled from 'styled-components'
-
-const IconButton = styled.button`
-  background: none;
-  border: none;
-  border-radius: 10px;
-  padding: 5px 0 0 5px;
-
-  :active {
-    filter: saturate(5);
-  }
-
-  :focus {
-    outline: none;
-    box-shadow: 0 0 0 2px ${theme.text};
-  }
-`
+import { IconButton } from './Button'
 
 export default function SearchButton(props) {
   const { onClick } = props
