196da5d422dc473b75642bd9e6ab2db5074f12e6
halyngoc <lyha@pdx.edu>
2020-06-01 06:07:27 +0000

    Deploying to gh-pages from  @ dfed57489ab85a2a70cd6a29a1ea1854e24c0c08 🚀

---
+++ precache-manifest.9e1f56e4a74be1a3294789b07cf49c90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72279b259e8c4b308115ccb9a673fbc",
+    "url": "/recipedia/index.html"
+  },
+  {
+    "revision": "df5067ef1dd14b87a26a",
+    "url": "/recipedia/static/js/2.8df008bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipedia/static/js/2.8df008bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eacd6d1e426dc7f6d3cd",
+    "url": "/recipedia/static/js/main.72be0428.chunk.js"
+  },
+  {
+    "revision": "856688276154979f23b4",
+    "url": "/recipedia/static/js/runtime-main.942e0537.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/recipedia/precache-manifest.3ebb786249bfbc653b2e16fcc1455180.js"
+  "/recipedia/precache-manifest.9e1f56e4a74be1a3294789b07cf49c90.js"
 );
 
 self.addEventListener('message', (event) => {
