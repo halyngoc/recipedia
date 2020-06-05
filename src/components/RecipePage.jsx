a89e5c1b7df0a35ac35b14b9dbd5eacb3da291f9
Ha Ly <lyha@pdx.edu>
2020-06-04 23:46:05 -0700

    Fix recipe page button being on the same line as recipe cards sometimes

--- src/components/RecipePage.jsx
+++ src/components/RecipePage.jsx
@@ -7,12 +7,21 @@ const RecipePageContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+
+  > div {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-bottom: 1rem;
+  }
 `
 
 export default function RecipePage({ recipes, onMoreClick }) {
   return (
     <RecipePageContainer>
-      {recipes.map((recipe, index) => <RecipeCard recipe={recipe} key={`recipe-page-card-${index}`} />)}
+      <div>
+        {recipes.map((recipe, index) => <RecipeCard recipe={recipe} key={`recipe-page-card-${index}`} />)}
+      </div>
       <Button onClick={onMoreClick}>See more</Button>
     </RecipePageContainer>
   )
