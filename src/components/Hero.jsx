939ff5821b1f4b22bdea11d9d053ee8b2c46cb2b
Ha Ly <lyha@pdx.edu>
2020-06-06 01:53:12 -0700

    Add username form

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { useDevice, useDate } from '../util'
 import { theme } from '../global'
 import { Button } from './buttons/Button'
 import EditButton from './buttons/EditButton'
+import CheckButton from './buttons/CheckButton'
 
 const HeroContainer = styled.div`
   text-align: center;
 
   h1 {
     font-weight: normal;
-    margin: 0;
-  }
+    margin: 0 0 0.2rem 0;
+    display: inline-flex;
+    flex-wrap: wrap;
+    align-items: baseline;
+    justify-content: center;
 
-  span {
-    color: ${theme.accent};
+    span {
+      color: ${theme.accent};
+      margin-left: 0.5rem;
+    }
+
+    input {
+      background-color: #00000000;
+      color: ${theme.accent};
+      border: 2px solid ${theme.accent};
+      border-radius: 10px;
+      font-size: 1.5rem;
+      outline: none;
+      margin-left: 0.5rem;
+    }
   }
 
   button {
     margin: 0.5em 0.25em 0 0.25rem;
   }
+
+  p {
+    margin: 0 0 0.2rem 0;
+  }
 `
+
+function Username({ name, onChangeUsername }) {
+  const [nameFieldValue, setNameFieldValue] = useState('')
+  const [mode, setMode] = useState('view')
+
+  const onFormSubmit = event => {
+    event.preventDefault()
+    onChangeUsername(nameFieldValue)
+    setMode('view')
+  }
+
+  if (mode === 'edit') {
+    return (
+      <form onSubmit={onFormSubmit}>
+        <input type="text" defaultValue={name} onChange={e => setNameFieldValue(e.target.value)} />
+        <CheckButton onClick={onFormSubmit} />
+      </form>
+    )
+  } else {
+    return (
+      <>
+        <span>{name}</span>
+        <EditButton onClick={() => setMode('edit')} />
+      </>
+    )
+  }
+}
 
 export default function Hero({ name, onChangeUsername, onBrowseClick, onSeeFavoritesClick }) {
   const device = useDevice()
@@ -39,8 +86,8 @@ export default function Hero({ name, onChangeUsername, onBrowseClick, onSeeFavor
 
   return (
     <HeroContainer device={device}>
-      <h1>Good {timeOfDay()}, <span>{name}</span><EditButton /></h1>
-      {dateTime}
+      <h1>Good {timeOfDay()}, <Username name={name} onChangeUsername={onChangeUsername} /></h1>
+      <p>{dateTime}</p>
       <div>
         <Button device={device} variant="primary" onClick={onBrowseClick}>Browse recipes</Button>
         <Button device={device} variant="secondary" onClick={onSeeFavoritesClick}>See my favorites</Button>
