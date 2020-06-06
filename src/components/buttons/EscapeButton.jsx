65e6305d9a02d75f0bee0f859710db1314d07e8f
Ha Ly <lyha@pdx.edu>
2020-06-05 17:48:18 -0700

    Add escape icon button

---
+++ src/components/buttons/EscapeButton.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import 'boxicons'
+import { theme } from '../../global'
+import { useDevice } from '../../util'
+import styled from 'styled-components'
+
+// Override ./IconButton
+export const IconButton = styled.button`
+  background: none;
+  border: none;
+  border-radius: 10px;
+  padding: 0;
+
+  :active {
+    filter: saturate(5);
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${theme.text};
+  }
+`
+
+export default function EscapeButton({ onClick }) {
+  const device = useDevice()
+
+  return (
+    <IconButton onClick={onClick}>
+      <box-icon
+        name="x"
+        color={theme.text}
+        size={device === 'mobile' ? '2.5rem' : '3.5rem'}
+      />
+    </IconButton>
+  )
+}
